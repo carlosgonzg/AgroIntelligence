64f913795f82c0f6f6346e27e5c48864bf42b2f3
Carlos Gonzalez <carltronik@outlook.com>
2016-03-31 15:42:59 -0400

    First commit of the app. Dividing into three projects , app, bll (services) and dal (model)

---
+++ agrointelligence.bll/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace agrointelligence.bll
+{
+    public class Class1
+    {
+    }
+}
---
+++ agrointelligence.dal/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace agrointelligence.dal
+{
+    public class Class1
+    {
+    }
+}
